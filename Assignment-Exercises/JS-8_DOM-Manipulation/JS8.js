85cb3b81ae0c3d4fb47b6d9e6a9101d1385bb91f
John Carlo Aguilar <johncarlo.aguilar104@gmail.com>
2025-02-14 15:06:25 +0800

    Update JS8.js

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,14 @@
 // your code starts here
+
+
+var city; // Define a string here, e.g., "New York"
+var temperature; // Define a number here, e.g., 22.5
+var hasPets; // Define a boolean here, e.g., true or false
+var colors; // Define an array here, e.g., ["red", "green", "blue"]
+var none
+
+// Checking types and values
+console.log(typeof city); // Output: should be "string" (until a value is assigned)
+console.log(typeof temperature);  // Output: should be "number" (until a value is assigned)
+console.log(typeof hasPets); // Output: should be "boolean" (until a value is assigned)
+console.log(typeof colors); // Output: should be "object" (until a value is assigned).
